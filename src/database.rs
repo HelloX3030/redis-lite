6c8b362928849d24a87d6a1920592934bea047f3
lseeger <lseeger@student.42.fr>
2025-07-25 14:22:37 +0200

    added basic hash map saving

---
+++ src/database.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+pub struct Store {
+    map: HashMap::<String, String>,
+}
+
+impl Store {
+    pub fn new() -> Store {
+        Store { map: HashMap::new() }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+
+    pub fn get(&mut self, key: &str) -> Option<&mut String> {
+        self.map.get_mut(key)
+    }
+
+    pub fn print(&self) {
+        for (key, value) in self.map.iter(){
+            println!("[{}]: {}", key, value);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod database;
+
+use database::Store;
+
 fn main() {
-    println!("Hello, world!");
+    let mut store: Store = Store::new();
+    store.set("Spaten".to_string(), "Tool".to_string());
+    store.set("ABC".to_string(), "Alphabet".to_string());
+    store.set("XXXXXXXX".to_string(), "Interesting".to_string());
+    store.set("Spaten".to_string(), "almighty god".to_string());
+    if let Some(spaten) = store.get("Spaten") {
+        println!("Der Wahre Spaten: {}", spaten);
+    }
+    store.print();
 }
